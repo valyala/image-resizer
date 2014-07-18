8c422ce20165fe6829fd546b4c315b996894ee3a
Aliaksandr Valialkin <valyala@gmail.com>
2014-07-18 12:05:34 +0300

    blobcache optimization: use a single memcache item for storing small blobs

--- blobcache/blobcache.go
+++ blobcache/blobcache.go
@@ -18,18 +18,26 @@ const (
 )
 
 func Set(c appengine.Context, item *memcache.Item) (err error) {
+	if isSmallValue(len(item.Value)) {
+		if err = memcache.Set(c, item); err != nil {
+			c.Errorf("Error storing item in memcache under key=%s: %v", item.Key, err)
+			return err
+		}
+		return nil
+	}
+
+	valueSize := uint64(len(item.Value))
 	checksum := crc64.Checksum(item.Value, crc64Table)
 	chunkItem := *item
-	valueSize := uint64(len(item.Value))
 	for i := uint64(0); i < valueSize; i += MAX_CHUNK_SIZE {
-		chunkItem.Key = fmt.Sprintf("%016X%016X%s", checksum, i, item.Key)
+		chunkItem.Key = getChunkKey(checksum, i, item.Key)
 		iNext := i + MAX_CHUNK_SIZE
 		if iNext > valueSize {
 			iNext = valueSize
 		}
-		chunkItem.Value = item.Value[i : iNext]
+		chunkItem.Value = item.Value[i:iNext]
 		if err = memcache.Set(c, &chunkItem); err != nil {
-			c.Errorf("Error storing chunkItem[%016X:%016X] in memcache under key=%s: %v", i, iNext, chunkItem.Key, err)
+			c.Errorf("Error storing chunkItem[%016X] in memcache under key=%s: %v", i, chunkItem.Key, err)
 			return err
 		}
 	}
@@ -50,6 +58,10 @@ func Get(c appengine.Context, key string) (item *memcache.Item, err error) {
 		}
 		return nil, err
 	}
+	if isSmallValue(len(masterItem.Value)) {
+		return masterItem, nil
+	}
+
 	var checksum, valueSize uint64
 	n, err := fmt.Sscanf(string(masterItem.Value), "%016X%016X", &checksum, &valueSize)
 	if err != nil {
@@ -63,17 +75,17 @@ func Get(c appengine.Context, key string) (item *memcache.Item, err error) {
 
 	var chunks [][]byte
 	for i := uint64(0); i < valueSize; i += MAX_CHUNK_SIZE {
-		chunkKey := fmt.Sprintf("%016X%016X%s", checksum, i, key)
+		chunkKey := getChunkKey(checksum, i, key)
 		chunkItem, err := memcache.Get(c, chunkKey)
 		if err != nil {
-			c.Errorf("Error when obtaining chunkItem[%016X:%016X] for key=%s: %v", i, i+MAX_CHUNK_SIZE, chunkKey, err)
+			c.Errorf("Error when obtaining chunkItem[%016X] for key=%s: %v", i, chunkKey, err)
 			return nil, memcache.ErrCacheMiss
 		}
 		if i+MAX_CHUNK_SIZE <= valueSize && uint64(len(chunkItem.Value)) != MAX_CHUNK_SIZE {
-			c.Errorf("Unexpected length for chunkItem[%016X:016X] for key=%s: %d. Expected %d", len(chunkItem.Value), MAX_CHUNK_SIZE)
+			c.Errorf("Unexpected length for chunkItem[%016X] for key=%s: %d. Expected %d", i, chunkKey, len(chunkItem.Value), MAX_CHUNK_SIZE)
 			return nil, memcache.ErrCacheMiss
 		} else if i+MAX_CHUNK_SIZE > valueSize && uint64(len(chunkItem.Value)) != valueSize-i {
-			c.Errorf("Unexpected length for chunkItem[%016X:016X] for key=%s: %d. Expected %d", len(chunkItem.Value), valueSize-i)
+			c.Errorf("Unexpected length for chunkItem[%016X] for key=%s: %d. Expected %d", i, chunkKey, len(chunkItem.Value), valueSize-i)
 			return nil, memcache.ErrCacheMiss
 		}
 		chunks = append(chunks, chunkItem.Value)
@@ -82,4 +94,12 @@ func Get(c appengine.Context, key string) (item *memcache.Item, err error) {
 	item = masterItem
 	item.Value = bytes.Join(chunks, []byte{})
 	return item, nil
+}
+
+func isSmallValue(valueSize int) bool {
+	return valueSize != 32 && valueSize <= int(MAX_CHUNK_SIZE)
+}
+
+func getChunkKey(checksum, i uint64, key string) string {
+	return fmt.Sprintf("%016X%016X%s", checksum, i, key)
 }
